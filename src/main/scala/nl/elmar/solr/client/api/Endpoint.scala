5f8ad220301a094ce78b3a846b79fdee66aed817
lavrov <lavrovvv@gmail.com>
2017-09-15 15:47:11 +0200

    Rename call -> toHttpRequest to make it clear what this method does

--- src/main/scala/nl/elmar/solr/client/api/Endpoint.scala
+++ src/main/scala/nl/elmar/solr/client/api/Endpoint.scala
@@ -3,5 +3,5 @@ package nl.elmar.solr.client.api
 import akka.http.scaladsl.model.HttpRequest
 
 trait Endpoint[Input] {
-  def call(input: Input): HttpRequest
+  def toHttpRequest(input: Input): HttpRequest
 }
